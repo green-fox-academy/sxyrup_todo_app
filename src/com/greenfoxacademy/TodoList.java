453b7d1399baefbba1fbd2e8eb781e85589e74ad
Dora Ree <daura.ree@gmail.com>
2020-12-17 21:26:09 +0100

    make todolist class implement serializable. add new functions. fomrat code.

--- src/com/greenfoxacademy/TodoList.java
+++ src/com/greenfoxacademy/TodoList.java
@@ -1,34 +1,43 @@
 package com.greenfoxacademy;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-//Todo implements iterable, iterator method?
-public class TodoList {
-    private List<Todo> todos;
 
-    public TodoList(ArrayList<Todo> todos) {
-        this.todos = todos;
-    }
+public class TodoList implements Serializable { // implements SERIALIZABLE
+  private List<Todo> todos;
 
-    public TodoList() {
-        this(new ArrayList<>());
-    }
+  public TodoList(ArrayList<Todo> todos) {
+    this.todos = todos;
+  }
 
+  public TodoList() {
+    this(new ArrayList<>());
+  }
 
-    void addNew(String description) {
-        todos.add(new Todo(description));
-    }
-    int count(){
-        return todos.size();
-    }
+  void addNew(String description) {
+    todos.add(new Todo(description));
+  }
 
-    @Override
-    public String toString() {
-        String t = "";
-        for (int i = 0; i < todos.size(); i++) {
-            t += (i + 1) + ". " + todos.get(i) + "\n";
-        }
-        return t;
+  void remove(int id) throws IndexOutOfBoundsException {
+    todos.remove(id - 1);
+  }
+
+  void complete(int id) throws IndexOutOfBoundsException {
+    todos.get(id - 1).complete();
+  }
+
+  int count() {
+    return todos.size();
+  }
+
+  @Override
+  public String toString() {
+    String t = "";
+    for (int i = 0; i < todos.size(); i++) {
+      t += (i + 1) + ". " + todos.get(i) + "\n";
     }
+    return t;
+  }
 }
