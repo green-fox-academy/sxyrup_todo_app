363751c679eb77fe160bbc973a61d3826585adb0
Dora Ree <daura.ree@gmail.com>
2020-12-17 21:20:27 +0100

    format main, add todo note

--- src/com/greenfoxacademy/Main.java
+++ src/com/greenfoxacademy/Main.java
@@ -4,15 +4,15 @@ import java.io.IOException;
 import java.nio.file.NoSuchFileException;
 
 public class Main {
-    public static void main(String[] args) {
-        TodoApp app = new TodoApp();
-        ArgHandler argHandler = new ArgHandler(app);
-        try {
-            argHandler.parse(args);
-        } catch (NoSuchFileException n) {
-            System.out.println("no such file");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+  public static void main(String[] args) {
+    TodoApp app = new TodoApp();
+    ArgHandler argHandler = new ArgHandler(app);
+    try {
+      argHandler.parse(args); // TODO: CATCH INDEX OUT OF BOUNDS e,  /NUMBER FORMAT EXCEPTION (REMOVE, COMPLETE..)
+    } catch (NoSuchFileException n) {
+      System.out.println("no such file");
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 }
