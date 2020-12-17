72d4c825dd3ef2077428e9327c341bdf941ba953
Dora Ree <daura.ree@gmail.com>
2020-12-17 21:22:05 +0100

    make description final. format code.

--- src/com/greenfoxacademy/Todo.java
+++ src/com/greenfoxacademy/Todo.java
@@ -3,21 +3,20 @@ package com.greenfoxacademy;
 import java.io.Serializable;
 
 public class Todo implements Serializable {
-    private int id; //ToDo getID > Files.lines(Paths.get(path)).count()
-    private String description;
-    private boolean isDone;
+  private final String DESCRIPTION;
+  private boolean isDone;
 
-    public Todo(String description) {
-        this.description = description;
-        this.isDone = false;
-    }
+  public Todo(String description) {
+    this.DESCRIPTION = description;
+    this.isDone = false;
+  }
 
-    void complete() {
-        this.isDone = true;
-    }
+  void complete() {
+    this.isDone = true;
+  }
 
-    @Override
-    public String toString() {
-        return (isDone ? " [X] " : " [ ] ") + description;
-    }
+  @Override
+  public String toString() {
+    return (isDone ? " [X] " : " [ ] ") + DESCRIPTION;
+  }
 }
