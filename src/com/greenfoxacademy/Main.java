b3915e2a7b94e407d8ddc003674032c18839149e
Dora Ree <daura.ree@gmail.com>
2020-12-19 03:38:32 +0100

    add invalid argument msg

--- src/com/greenfoxacademy/Main.java
+++ src/com/greenfoxacademy/Main.java
@@ -8,16 +8,15 @@ public class Main {
     TodoApp app = new TodoApp();
     ArgHandler argHandler = new ArgHandler(app);
     try {
-      argHandler.parse(args); // TODO: CATCH INDEX OUT OF BOUNDS e,  /NUMBER FORMAT EXCEPTION (REMOVE, COMPLETE..)
+      argHandler.parse(
+          args); // TODO: CATCH INDEX OUT OF BOUNDS e,  /NUMBER FORMAT EXCEPTION (REMOVE, COMPLETE..)
     } catch (NoSuchFileException n) {
       System.out.println("no such file");
     } catch (IOException e) {
       e.printStackTrace();
-    }
-    catch (NumberFormatException n) {
-      System.out.println("");
-    }
-    catch (IndexOutOfBoundsException e) {
+    } catch (NumberFormatException n) {
+      System.out.println("Nem lehetseges a feladat vegrehajtasa: a megadott index nem szam!");
+    } catch (IndexOutOfBoundsException e) {
       System.out.println("Nem lehetseges a feladat vegrehajtasa, Tulindexelesi problema adodott!");
     }
   }
